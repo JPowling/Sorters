be57dc4b36faac2d577ed5f7160e2b4a0a6518bb
Jens Rosenbauer <jens.rosenbauer@t-online.de>
2021-01-27 14:27:45 +0100

    in Start Algorithmen Alphabetisch angeordnet

--- src/sortalgorithms/Start.java
+++ src/sortalgorithms/Start.java
@@ -7,9 +7,9 @@ import algorithmen.SelectionSort;
 public class Start {
 
 	public static void main(String[] args) {
-		new SelectionSort();
 		new BubbleSort();
 		new InsertionSort();
+		new SelectionSort();
 		new GUI_Sort();
 	}
 
