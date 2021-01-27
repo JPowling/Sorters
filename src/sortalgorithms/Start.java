f791d6c254715419dfc734aa211aa3020b389e07
Jens Rosenbauer <55103045+Jens988@users.noreply.github.com>
2021-01-27 10:32:44 +0100

    Update Start.java

--- src/sortalgorithms/Start.java
+++ src/sortalgorithms/Start.java
@@ -5,9 +5,9 @@ public class Start {
 	public static void main(String[] args) {
 
 		//oldSource.GUI_Sortierverfahren sortieren = new oldSource.GUI_Sortierverfahren();
-		new SelectionSort();
 		new BubbleSort();
 		new InsertionSort();
+		new SelectionSort();
 		new GUI_Algos();
 	}
 
