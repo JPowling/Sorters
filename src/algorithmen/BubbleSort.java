763c9e8f05c1d7cc726b99cd6abb7dfc34fdbc9f
Jens Rosenbauer <jens.rosenbauer@t-online.de>
2021-01-30 16:32:18 +0100

    fix BubbleSort Bug #54

--- src/algorithmen/BubbleSort.java
+++ src/algorithmen/BubbleSort.java
@@ -5,7 +5,7 @@ import sortalgorithms.Algorithmus;
 public class BubbleSort extends Algorithmus {
 
     public void internalSort() {
-        for(int end = daten.length - 1; end > 1; end--) {
+        for (int end = daten.length - 1; end > 0; end--) {
             for (int i = 0; i < end; i++) {
                 if (compare(i, i + 1)) {
                     swap(i, i + 1);
